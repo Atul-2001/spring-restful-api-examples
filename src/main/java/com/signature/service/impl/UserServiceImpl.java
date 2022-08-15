5c16e1daa00b5a0077394cb435aca9d574774fc7
Atendra Suryvanshi <suryavanshi.atendra15@gmail.com>
2022-08-15 16:09:03 +0530

    adding uri component builder

--- src/main/java/com/signature/service/impl/UserServiceImpl.java
+++ src/main/java/com/signature/service/impl/UserServiceImpl.java
@@ -2,27 +2,37 @@ package com.signature.service.impl;
 
 import com.signature.model.User;
 import com.signature.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+  private final String API_URL;
   private final RestTemplate restTemplate;
 
-  public UserServiceImpl(RestTemplate restTemplate) {
+  public UserServiceImpl(@Value("${api.url}") String api_url,
+                         final RestTemplate restTemplate) {
+    this.API_URL = api_url;
     this.restTemplate = restTemplate;
   }
 
   @Override
   public User getUserById(int id) {
-    return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users/{id}", User.class, id);
+    UriComponents uriComponents = UriComponentsBuilder.fromUriString(API_URL)
+            .path("/users/{id}").buildAndExpand(id);
+    return restTemplate.getForObject(uriComponents.toUriString(), User.class);
   }
 
   @Override
   public List<User> getAll(int limit) {
-    return restTemplate.getForObject("https://jsonplaceholder.typicode.com/users?_limit={limit}", List.class, limit);
+    UriComponents uriComponents = UriComponentsBuilder.fromUriString(API_URL)
+            .path("/users").queryParam("_limit", limit).build();
+    return restTemplate.getForObject(uriComponents.toUriString(), List.class);
   }
 }
